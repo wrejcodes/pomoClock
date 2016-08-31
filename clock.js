ddaa79da62e097bb66475d00bd3ff99583e83c47
Will Jackson <wrejcodes@gmail.com>
2016-08-30 21:23:08 -0500

    got the timer working

--- clock.js
+++ clock.js
@@ -1,15 +1,37 @@
 var screenText = document.getElementById("timer-label");
 
-var timer = {
-	secondsRemaining: 25*60
+var state = {
+	WORK:1,
+	REST:0,
 };
 
-timer.secondsRemaining -= 3;
+var timer = {
+	secondsRemaining: 25*60,
+	state: state.WORK,
+	workTime:25*60,
+	breakTime: 5*60
+};
 
-var minutes = Math.floor(timer.secondsRemaining/60);
-var seconds = timer.secondsRemaining%60;
-
-screenText.innerHTML = `${minutes}:${seconds < 10 ? '0' 
+var setLabel = function(){
+	var minutes = Math.floor(timer.secondsRemaining/60);
+	var seconds = timer.secondsRemaining%60;
+	screenText.innerHTML = `${minutes}:${seconds < 10 ? '0' 
 						+ seconds.toString(): seconds}`;
+};
+
+setLabel();
+
+var updateLabel = function(){
+	timer.secondsRemaining -= 1;
+	var minutes = Math.floor(timer.secondsRemaining/60);
+	var seconds = timer.secondsRemaining%60;
+	screenText.innerHTML = `${minutes}:${seconds < 10 ? '0' 
+						+ seconds.toString(): seconds}`;
+};
+
+var countdown = window.setInterval(updateLabel,1000);
+
+
+
 
 
