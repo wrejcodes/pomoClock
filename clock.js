22f2532eafe6f941715db9ffd9d7defbc361df14
Will Jackson <wrejcodes@gmail.com>
2016-09-04 20:25:06 -0500

    added a pause/play functionality for the timer

--- clock.js
+++ clock.js
@@ -1,5 +1,7 @@
 var screenText = document.getElementById("timer-label");
 var statusLabel = document.getElementById("status-label");
+var startButton = document.getElementById("start");
+
 
 var state = {
 	WORK:1,
@@ -10,7 +12,9 @@ var timer = {
 	secondsRemaining: 25*60,
 	state: state.WORK,
 	workTime: 25*60,
-	breakTime: 5*60
+	breakTime: 5*60,
+	running: false,
+	ID: null
 };
 
 
@@ -34,8 +38,18 @@ var updateLabel = function(){
 						+ seconds.toString(): seconds}`;
 };
 
-var countdown = window.setInterval(updateLabel,1000);
 
+var startTimer = function(){
+	if(!timer.running){
+		timer.ID = window.setInterval(updateLabel,1000);
+		timer.running = true;
+	} else {
+		clearTimeout(timer.ID);
+		timer.running = false;
+	}
+};
+
+startButton.addEventListener("click",startTimer,false);
 
 
 
